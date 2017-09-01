7c069cdbc39e72dac9ff90dae63f0c2e130f48ba
Vinit Shahdeo <vinitshahdeo@gmail.com>
2017-09-01 19:27:05 +0530

    LED Blinking

---
+++ led_blink.ino
@@ -0,0 +1,22 @@
+int LED_h=10;
+int LED_l=9;
+
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(LED_h,OUTPUT);
+  pinMode(LED_l,OUTPUT);
+  digitalWrite(LED_h,LOW);
+  digitalWrite(LED_l,LOW);
+  
+  
+}
+
+void loop() {
+
+digitalWrite(LED_h,HIGH);
+Serial.println("Led is ON!!!")
+delay(2000);
+digitalWrite(LED_h,LOW);
+Serial.println("LED is OFF");
+}
